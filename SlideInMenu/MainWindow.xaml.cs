65c1187583933fe2453884d48f65c0103b68612b
takada.tentaro <takadatentarou@gmail.com>
2020-04-29 13:10:08 +0900

    ソース登録

---
+++ SlideInMenu/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlideInMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var menuWindow = new MenuWindow();
+            menuWindow.SelectedButtonEvent += Wn_SelectedButtonEvent;
+            menuWindow.ShowSlideWindow(this.Left,this.Top + 20,this.Width);
+        }
+
+        private void Wn_SelectedButtonEvent(object sender, string e)
+        {
+            tb.Text = $"{e}ボタンが押されました";
+        }
+    }
+}
---
+++ SlideInMenu/MenuWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SlideInMenu
+{
+    /// <summary>
+    /// MenuWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        /// <summary>
+        /// このウィンドウ上のどのボタンが押されたかを親ウィンドウに通知するイベント
+        /// </summary>
+        public event EventHandler<string> SelectedButtonEvent;
+
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        public async void ShowSlideWindow(double left, double top, double ownerWidth)
+        {
+            this.Top = top;
+            this.Left = left;
+            this.Show();
+
+            // スライドイン表示
+            for (int i = 1; i < 15; i++)
+            {
+                var newValue = this.Width + 30 * (i - 0.7);
+                Console.WriteLine(newValue);
+                if (newValue <= ownerWidth)
+                {
+                    
+                    this.Width = newValue;
+                }
+                else 
+                {
+                    break;
+                }
+                await Task.Delay(1);
+            }
+            this.Width = ownerWidth;
+        }
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            SelectedButtonEvent?.Invoke(this, (sender as Button)?.Name);
+
+            // スライドアウト表示
+            for (int i = 1; i < 15; i++)
+            {
+                var newValue = this.Width - 30 * (i - 0.7);
+                if (newValue >= 0)
+                {
+                    this.Width = newValue;
+                }
+                else 
+                {
+                    break;
+                }
+
+                await Task.Delay(5);
+            }
+            this.Visibility = Visibility.Collapsed;
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
